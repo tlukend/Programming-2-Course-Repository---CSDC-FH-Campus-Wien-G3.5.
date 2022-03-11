bf7f8cd55ca41287c5fd072bc1ae2c9e3d50a491
andelamaks <maksimovic34@gmail.com>
2022-03-11 17:11:55 +0100

    Javafx works now :)

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,9 +1,39 @@
 package at.ac.fhcampuswien;
 
-public class App {
-    public static void main(String[] args) {
-       Menu.printMenu();
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    //Variables
+    protected final static int APP_SIZE = 860;
+
+    //GUI Variables
+    protected static Pane root;
+    protected static Scene menuScene;
+    protected static Button a, b, y, q;
+
+    public static void main(String[] args) { launch(args); }
+       //Menu.printMenu();
         //Menu.start();
+       @Override
+       public void start(Stage primaryStage) {
+        primaryStage.setTitle("N E W S A P P");
+        //primaryStage.getIcons().add(new Image(getClass().getResource("").toExternalForm()));
+        primaryStage.setScene(menuScene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        //App.root.getChildren().addAll(App.snake.getSnakeLengthArr());
+
+           //sceneGame = new Scene(layoutGame, GAME_SIZE, GAME_SIZE);
     }
 }
         //die App Klasse enthält die main() Funktion und
