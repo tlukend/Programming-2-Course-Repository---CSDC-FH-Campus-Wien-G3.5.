98b8ca0ec5d4a4c07fa9ee84b88aa7942bfe2252
Rasuli Faryal <farial_rasuli@hotmail.com>
2022-04-21 19:44:12 +0200

    Nes Response: wurde daran gearbeitet mal für den Anfang Attribute wurden hinzugefügt

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -5,6 +5,13 @@ import javafx.scene.Node;
 public class Article {
     private String author;
     private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+    //sorce? soll es drinnen sein oder nicht?
 
     public Article(String author, String title) {
         this.author = author;
@@ -22,6 +29,54 @@ public class Article {
     @Override
     public String toString() {
         return "Author: " + getAuthor() + ", Title: " + getTitle();
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
 
--- src/main/java/at/ac/fhcampuswien/NewsRespone.java
+++ src/main/java/at/ac/fhcampuswien/NewsRespone.java
@@ -1,4 +1,22 @@
 package at.ac.fhcampuswien;
 
+import java.util.List;
+
 public class NewsRespone {
+    String status;
+    int totalResults;
+    List<Article> articles;
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public List<Article> getArticles(){
+        return articles;
+    }
 }
