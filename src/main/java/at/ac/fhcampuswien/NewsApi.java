f993bb86b986310792f4556ce32ef124a8d3b40f
Slavica-lab <slavica-alvir@hotmail.com>
2022-04-22 05:20:33 +0200

    NewsApi

--- src/main/java/at/ac/fhcampuswien/NewsApi.java
+++ src/main/java/at/ac/fhcampuswien/NewsApi.java
@@ -1,6 +1,30 @@
 package at.ac.fhcampuswien;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class NewsApi {
 
+    public static void main(String[] args) {
+        try {
+         System.out.println(run("https://newsapi.org/v2/everything?q=bitcoin&apiKey= f970a93f427c449d8a61d53e717fc78c")); //url sollte dynamisch generiert werden können
+        }catch (IOException e){
+        System.out.println("ERROR");
+        }
+    }
+    public static String run(String url)throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+            .url(url)
+            .build();
+
+  try (Response response = client.newCall(request).execute()) {
+        return response.body().string();
+    }
     
+}
 }
