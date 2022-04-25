ed49b9dbaba4289a5cd6967430a02fbc2dcda70f
Slavica-lab <slavica-alvir@hotmail.com>
2022-04-25 04:05:09 +0200

    if author null

--- src/main/java/at/ac/fhcampuswien/NewsResponse.java
+++ src/main/java/at/ac/fhcampuswien/NewsResponse.java
@@ -7,11 +7,11 @@ public class NewsResponse {
     private int totalResults;
     private List<Article> articles;
 
-    public NewsResponse (String status, int totalResults, List<Article> articles){
+   /* public NewsResponse (String status, int totalResults, List<Article> articles){
         this.status = status;
         this.totalResults = totalResults;
         this.articles = articles;
-    }
+    }*/
 
 
     public String getStatus() {
